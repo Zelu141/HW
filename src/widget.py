1845a3822210e60c7383db2284620884016b8b79
Zeluk <KolenskiyPython>
2024-06-16 17:13:42 +0700

    third fixies

--- src/widget.py
+++ src/widget.py
@@ -1,17 +1,24 @@
+from masks import get_mask_account
+from masks import get_mask_card_number
 from datetime import datetime
 
 
-def get_mask_account(account: str) -> str | None:
-    """Фукнция маскирующая номер счёта"""
-    if account.isdigit() and len(account) == 20:
-        return f"**{account[-4::]}"
+def mask_account_card(cards_number: str) -> str:
+    """Функция для маскировки номера карты или счёта"""
+    if "Счёт" in cards_number:
+        mask_account = f"Счёт {get_mask_account(cards_number[:])}"
     else:
-        return None
+        card = get_mask_card_number(cards_number[-16:])
+        mask_card = cards_number.replace(cards_number[-16:], card)
+        return mask_card
 
 
-def get_mask_card_number(card_number: str) -> str | None:
-    """Функция маскирующая номер счёта"""
-    if card_number.isdigit() and len(card_number) == 16:
-        return f"{card_number[:4]} {card_number[4:6]}** **** {card_number[12:]}"
-    else:
-        return None
+def get_data(data: str) -> str:
+    """Функция для возвращения даты в нужный формат"""
+    d = datetime.strptime(data, "%Y-%m-%dT%H:%M:%S.%f")
+    return d.strftime("%d.%m.%Y")
+
+
+if __name__ == "__main__":
+    print(mask_account_card("Visa Gold 5999414228426353"))
+    print(get_data("2018-07-11T02:26:18.671407"))
