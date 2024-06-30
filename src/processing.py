73e7b72eb702fb6a0919259fc48186b85e92f65e
Zeluk <KolenskiyPython>
2024-06-30 21:33:53 +0700

    add tests

--- src/processing.py
+++ src/processing.py
@@ -1,5 +1,15 @@
 from typing import Any, Dict, List
 
+import pytest
+
+
+inform_state = [
+    {"id": 41428829, "state": "EXECUTED", "date": "2019-07-03T18:35:29.512364"},
+    {"id": 939719570, "state": "EXECUTED", "date": "2018-06-30T02:08:58.425572"},
+    {"id": 594226727, "state": "CANCELED", "date": "2018-09-12T21:27:25.241689"},
+    {"id": 615064591, "state": "CANCELED", "date": "2018-10-14T08:21:33.419441"},
+]
+
 
 def filter_by_state(origin_list: List[Dict[str, Any]], state: str = "EXECUTED") -> List[Dict[str, Any]]:
     """
@@ -12,9 +22,10 @@ def filter_by_state(origin_list: List[Dict[str, Any]], state: str = "EXECUTED") 
     Возвращает:
         List[Dict[str, Any]]: Список отфильтрованных операций.
     """
-    return [operation for operation in origin_list if operation.get('state') == state]
+    return [operation for operation in origin_list if operation.get("state") == state]
 
 
+@pytest.fixture
 def sort_by_date(origin_list: List[Dict[str, Any]], reverse_list: bool = True) -> List[Dict[str, Any]]:
     """Функция для сортировки по доте в порядке убывания или возрастания"""
     sorted_list = sorted(origin_list, key=lambda d: d["date"], reverse=reverse_list)
