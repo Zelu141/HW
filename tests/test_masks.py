1f9ca36277cc04daa56597f20fd41b8fd66492d4
Zeluk <KolenskiyPython>
2024-07-01 19:46:44 +0700

    more tests

--- tests/test_masks.py
+++ tests/test_masks.py
@@ -3,13 +3,11 @@ import pytest
 from src.masks import get_mask_card_number, get_mask_account
 
 
-@pytest.mark.parametrize(
-    "card_number, expected",
-    [
+@pytest.mark.parametrize("card_number, expected",[
         ("7000792289606361", "7000 79** **** 6361"),
-        ("7000792289606361", None),  # Неверная длина
+        ("70007922896063", None),  # Неверная длина
         ("abcd7000792289606361", None),  # Содержит не только цифры
-    ],
+    ]
 )
 def test_get_mask_card_number(card_number, expected):
     assert get_mask_card_number(card_number) == expected
